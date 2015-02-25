40ff7059de150de3e28689f1e00f6f054acd8494
Subhajit Sahu <wolfram77@gmail.com>
2015-02-25 11:21:57 +0530

    test code ready

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,20 @@
 // @wolfram77
 package main;
 
+// required modules
+import java.sql.*;
+import org.db.*;
+
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Connection conn = Db.conn();
+        Statement cmd = conn.createStatement();
+        ResultSet ans = cmd.executeQuery("SELECT * FROM tab");
+        while(ans.next())
+            System.out.println(ans.getString(1));
+        conn.close();
     }
 }
